9318516bfa040ce4133bef4549ac83256b9bbc2d
Edward Tanguay (hm.win) <edward@tanguay.info>
2020-11-22 17:57:32 +0100

    blank images

--- src/ArtistList.svelte
+++ src/ArtistList.svelte
@@ -23,7 +23,7 @@
         <div
           class="list-group-item d-flex w-100 list-group-item-action
           align-items-center">
-          <img class="rounded mr-3 d-block" {src} alt={`Photo of ${name}`} />
+          <img class="rounded mr-3 d-block" src="images/blank.jpg" alt={`Photo of ${name}`} />
           <div>
             <h4 class="mb-0">{name}</h4>
             <p class="text-muted mb-0">{reknown}</p>
